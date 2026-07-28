2deb9fe703a700945f73c2efcc48f1530d13ede4
kurashizu <kurashizu@users.noreply.github.com>
2026-07-29 02:02:00 +1000

    redesign favicon: cloud + K brand mark

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export const metadata: Metadata = {
     index: false,
     follow: false,
   },
+  icons: {
+    icon: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -28,9 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      </head>
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
